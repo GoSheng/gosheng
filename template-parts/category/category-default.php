32275b415d5f4560d146e31d2fbb19eed3f9cf6d
lin <469946668@qq.com>
2018-10-20 20:56:32 +0800

    -

--- template-parts/category/category-default.php
+++ template-parts/category/category-default.php
@@ -2,9 +2,14 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-if ( have_posts() ) :
-	while ( have_posts() ) : the_post();
-		get_template_part( 'template-parts/other/page', 'list' );
-	endwhile;
-endif;
-GoSheng_pagination();
+?>
+<div class="container bg-light pt-3">
+	<?php
+	if ( have_posts() ) :
+		while ( have_posts() ) : the_post();
+			get_template_part( 'template-parts/other/page', 'list' );
+		endwhile;
+	endif;
+	GoSheng_pagination();
+	?>
+</div>
