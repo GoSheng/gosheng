9c44978d24ea8a4a1d35dc9109dae2636b4074a7
lin <469946668@qq.com>
2018-10-20 21:33:43 +0800

    -

--- template-parts/other/reward.php
+++ template-parts/other/reward.php
@@ -7,48 +7,48 @@ global $GoSheng;
 <?php
 if ( $GoSheng['reward_all'] ) {
 	?>
-<!--    <div class="reward text-center mb-2 mb-lg-3">-->
-<!--		--><?php
+    <!--    <div class="reward text-center mb-2 mb-lg-3">-->
+    <!--		--><?php
 //		if ( $GoSheng['reward_alipay'] ) {
 //			?>
-<!--            <a class="btn btn-light btn-lg" id="reward_btn_alipay" href="#" data-toggle="modal"-->
-<!--               data-target="#reward_modal" data-modaltab="alipay">-->
-<!--                <i class="fab fa-alipay" aria-hidden="true">支付宝</i>-->
-<!--            </a>-->
-<!--		--><?php //} ?>
-<!--		--><?php
+    <!--            <a class="btn btn-light btn-lg" id="reward_btn_alipay" href="#" data-toggle="modal"-->
+    <!--               data-target="#reward_modal" data-modaltab="alipay">-->
+    <!--                <i class="fab fa-alipay" aria-hidden="true">支付宝</i>-->
+    <!--            </a>-->
+    <!--		--><?php //} ?>
+    <!--		--><?php
 //		if ( $GoSheng['reward_weixin'] ) {
 //			?>
-<!--            <a class="btn btn-light btn-lg" id="reward_btn_weixin" href="#" data-toggle="modal"-->
-<!--               data-target="#reward_modal" data-modaltab="weixin">-->
-<!--                <i class="fab fa-weixin" aria-hidden="true">微信</i>-->
-<!--            </a>-->
-<!--		--><?php //} ?>
-<!--		--><?php
+    <!--            <a class="btn btn-light btn-lg" id="reward_btn_weixin" href="#" data-toggle="modal"-->
+    <!--               data-target="#reward_modal" data-modaltab="weixin">-->
+    <!--                <i class="fab fa-weixin" aria-hidden="true">微信</i>-->
+    <!--            </a>-->
+    <!--		--><?php //} ?>
+    <!--		--><?php
 //		if ( $GoSheng['reward_qpay'] ) {
 //			?>
-<!--            <a class="btn btn-light btn-lg" id="reward_btn_qpay" href="#" data-toggle="modal"-->
-<!--               data-target="#reward_modal" data-modaltab="qpay">-->
-<!--                <i class="fab fa-qq" aria-hidden="true">QQ钱包</i>-->
-<!--            </a>-->
-<!--		--><?php //} ?>
-<!--		--><?php
+    <!--            <a class="btn btn-light btn-lg" id="reward_btn_qpay" href="#" data-toggle="modal"-->
+    <!--               data-target="#reward_modal" data-modaltab="qpay">-->
+    <!--                <i class="fab fa-qq" aria-hidden="true">QQ钱包</i>-->
+    <!--            </a>-->
+    <!--		--><?php //} ?>
+    <!--		--><?php
 //		if ( $GoSheng['reward_baifubao'] ) {
 //			?>
-<!--            <a class="btn btn-light btn-lg" id="reward_btn_baifubao" href="#" data-toggle="modal"-->
-<!--               data-target="#reward_modal" data-modaltab="baifubao">-->
-<!--                <i class="fab" aria-hidden="true">百度钱包</i>-->
-<!--            </a>-->
-<!--		--><?php //} ?>
-<!--		--><?php
+    <!--            <a class="btn btn-light btn-lg" id="reward_btn_baifubao" href="#" data-toggle="modal"-->
+    <!--               data-target="#reward_modal" data-modaltab="baifubao">-->
+    <!--                <i class="fab" aria-hidden="true">百度钱包</i>-->
+    <!--            </a>-->
+    <!--		--><?php //} ?>
+    <!--		--><?php
 //		if ( $GoSheng['reward_paypal'] ) {
 //			?>
-<!--            <a class="btn btn-light btn-lg" id="reward_btn_paypal" href="#" data-toggle="modal"-->
-<!--               data-target="#reward_modal" data-modaltab="paypal">-->
-<!--                <i class="fab fa-paypal" aria-hidden="true">PayPal</i>-->
-<!--            </a>-->
-<!--		--><?php //} ?>
-<!--    </div>-->
+    <!--            <a class="btn btn-light btn-lg" id="reward_btn_paypal" href="#" data-toggle="modal"-->
+    <!--               data-target="#reward_modal" data-modaltab="paypal">-->
+    <!--                <i class="fab fa-paypal" aria-hidden="true">PayPal</i>-->
+    <!--            </a>-->
+    <!--		--><?php //} ?>
+    <!--    </div>-->
     <div class="modal" id="reward_modal" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content bg-light">
@@ -112,8 +112,11 @@ if ( $GoSheng['reward_all'] ) {
 						<?php } ?>
 						<?php if ( $GoSheng['reward_paypal'] ) { ?>
                             <div role="tabpanel" class="tab-pane fade zoomIn animated" id="reward_paypal">
-                                <img class="img-fluid d-block mx-auto"
-                                     src="<?php echo $GoSheng['reward_paypal_image']['url']; ?>" alt="" title="">
+                                <!--                                <img class="img-fluid d-block mx-auto" src="-->
+								<?php //echo $GoSheng['reward_paypal_image']['url']; ?><!--" alt="" title="">-->
+                                <a href="https://paypal.me/<?php echo $GoSheng['reward_paypal_url']; ?>"
+                                   title="<?php esc_attr_e( 'PayPal.Me链接地址', 'GoSheng-framework' ) ?>"
+                                   target="_blank"><?php esc_html_e( '给我PayPal转账', 'GoSheng-framework' ) ?></a>
                                 <span class="d-block text-center"><?php echo $GoSheng['reward_paypal_text']; ?></span>
                             </div>
 						<?php } ?>
