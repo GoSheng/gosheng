602b91c014fdf76c26ac5822f6a73e006a46f58d
lin <469946668@qq.com>
2018-10-27 23:46:01 +0800

    -

--- inc/theme_update.php
+++ inc/theme_update.php
@@ -7,7 +7,7 @@ if ( file_exists( get_theme_file_path() . '/inc/theme-update/theme-update.php' )
 }
 
 $GoShengUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://raw.githubusercontent.com/zhang-chenglin/gosheng_update/master/theme.json',
+	'https://raw.githubusercontent.com/gosheng/gosheng_update/master/theme.json',
 	dirname( dirname( __FILE__ ) ) . '/functions.php',
 	'gosheng'
 );
