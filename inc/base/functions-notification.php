fde6800e61ee148613b3a0e741cd85878cdc3559
lin <469946668@qq.com>
2018-11-03 22:18:27 +0800

    -

--- inc/base/functions-notification.php
+++ inc/base/functions-notification.php
@@ -2,13 +2,18 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-global $GoSheng;
 
 function enqueue_notification() {
 	wp_register_script( 'notification', themeStaticFile_URI . 'js/notification.js', array(), THEME_STATIC_FILE_VERSION, true );
 	wp_enqueue_script( 'notification' );
 }
 
-if ( $GoSheng['notification_switch'] ) {
-	add_action( 'wp_enqueue_scripts', 'enqueue_notification' );
+add_action( 'redux/options/GoSheng/settings/change', 'GoSheng_notification' );
+if ( ! function_exists( 'GoSheng_notification' ) ) {
+	function GoSheng_notification() {
+		global $GoSheng;
+		if ( $GoSheng['notification_switch'] ) {
+			add_action( 'wp_enqueue_scripts', 'enqueue_notification' );
+		}
+	}
 }
