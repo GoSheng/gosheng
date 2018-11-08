82939be93db68ec0499fcb0a1efe0758dbf71c4a
lin <469946668@qq.com>
2018-11-08 17:55:30 +0800

    -

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,16 @@ gulp.task('script_local', function () {
         .pipe(gulp.dest('./static/js/'))
 });
 
+gulp.task('script_notyf', function () {
+    gulp.src('./static/js/notyf.js')
+        .pipe(uglify())
+        .on('error', function (err) {
+            util.log(util.colors.red('[Error]'), err.toString());
+        })
+        .pipe(rename('notyf.min.js'))
+        .pipe(gulp.dest('./static/js/'))
+});
+
 gulp.task('style_local', function () {
     gulp.src(['./static/css/local.css'])
         .pipe(cssnano())
@@ -25,13 +35,24 @@ gulp.task('style_local', function () {
         .pipe(gulp.dest('./static/css/'))
 });
 
+gulp.task('style_notyf', function () {
+    gulp.src(['./static/css/notyf.css'])
+        .pipe(cssnano())
+        .pipe(rename('notyf.min.css'))
+        .pipe(gulp.dest('./static/css/'))
+});
+
 gulp.task('default', ['clean'], function () {
     gulp.start('script_local', 'style_local')
     gulp.watch('static/css/local.css', ['style_local'])
+    gulp.watch('static/css/notyf.css', ['style_notyf'])
     gulp.watch('static/js/local.js', ['script_local'])
+    gulp.watch('static/js/notyf.js', ['script_notyf'])
 });
 
 gulp.task('watch', function () {
     gulp.watch('static/css/local.css', ['style_local'])
+    gulp.watch('static/css/notyf.css', ['style_notyf'])
     gulp.watch('static/js/local.js', ['script_local'])
+    gulp.watch('static/js/notyf.js', ['script_notyf'])
 });
