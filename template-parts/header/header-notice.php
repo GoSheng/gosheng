d760b8404c30df8a4d722d3735462269b044d13e
lin <469946668@qq.com>
2018-11-17 21:06:21 +0800

    -

--- template-parts/header/header-notice.php
+++ template-parts/header/header-notice.php
@@ -12,7 +12,7 @@ if ( $GoSheng['gosheng_notice'] ) {
              role="<?php esc_attr_e( '公告信息', 'GoSheng-framework' ) ?>">
             <i class="fas fa-volume-up mr-1 mr-lg-2 text-danger"></i>
             <span class="font-8 font-lg-10"><?php echo $GoSheng['gosheng_notice_text'] ?></span>
-            <button type="button" class="close" aria-label="<?php esc_attr_e( '关闭公告信息', 'GoSheng-framework' ); ?>">
+            <button class="close" aria-label="<?php esc_attr_e( '关闭公告信息', 'GoSheng-framework' ); ?>">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
