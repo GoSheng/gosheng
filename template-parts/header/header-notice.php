bff21925a3da7d2b4647c01ad4983e232b5e7b0f
lin <469946668@qq.com>
2018-11-17 21:10:27 +0800

    -

--- template-parts/header/header-notice.php
+++ template-parts/header/header-notice.php
@@ -8,8 +8,7 @@ global $GoSheng;
 if ( $GoSheng['gosheng_notice'] ) {
 	if ( ! GoSheng_isset_cookie( 'gosheng_notice_closed', 'yes' ) ) {
 		?>
-        <div id="gosheng_notice" class="alert alert-warning alert-dismissible fade show"
-             role="<?php esc_attr_e( '公告信息', 'GoSheng-framework' ) ?>">
+        <div id="gosheng_notice" class="alert alert-warning alert-dismissible fade show" role="<?php esc_attr_e( '公告信息', 'GoSheng-framework' ) ?>">
             <i class="fas fa-volume-up mr-1 mr-lg-2 text-danger"></i>
             <span class="font-8 font-lg-10"><?php echo $GoSheng['gosheng_notice_text'] ?></span>
             <button class="close" aria-label="<?php esc_attr_e( '关闭公告信息', 'GoSheng-framework' ); ?>">
