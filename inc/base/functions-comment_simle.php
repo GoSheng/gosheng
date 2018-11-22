c7dc2d40f23bc00d0c256a006927908ae96fa1d1
lin <469946668@qq.com>
2018-11-22 23:37:18 +0800

    尝试emoji表情

--- inc/base/functions-comment_simle.php
+++ inc/base/functions-comment_simle.php
@@ -12,35 +12,35 @@ if ( ! function_exists( 'GoSheng_comment_field_btn' ) ) {
 }
 if ( ! function_exists( 'simle_yan' ) ) {
 	function simle_yan() {
-		$simle   = '<div id="simle_yan" class="mt-1 d-flex flex-wrap">%1$s</div>';
-		$simle_a = '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="得意"><(￣︶￣)></span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="乾杯">[]~(￣▽￣)~*</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="啊啊">w(ﾟДﾟ)w</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="擦眼泪">(ノへ￣、)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="不屑">(￣_,￣ )</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="好耶">ヽ(✿ﾟ▽ﾟ)ノ</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="棒">(๑•̀ㅂ•́)و✧</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="抽">(￣ε(#￣)☆╰╮o(￣皿￣///)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="喂">(#`O′)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="亲">（づ￣3￣）づ╭❤～</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="汗">Σ( ° △ °|||)︴</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="笨">(～￣(OO)￣)ブ</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="擦">凸(艹皿艹 )</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="啵啵">(* ￣3)(ε￣ *)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="挖鼻屎">(*￣rǒ￣)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="嗷">┗|｀O′|┛ 嗷~~</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="好滴">(u‿ฺu✿ฺ)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="飞">︿(￣︶￣)︿</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="hi">Hi~ o(*￣▽￣*)ブ</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="啦啦">♪(^∇^*)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="拍桌">o(*≧▽≦)ツ┏━┓</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="惊喜">╰(*°▽°*)╯</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="嘟嘴">（○｀ 3′○）</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="乖">o(*^＠^*)o</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="愣住">(°ー°〃)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="放屁">○|￣|_ =3</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="哼哼">o(￣ヘ￣o＃)</span>';
-		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="可恶">（＝。＝）</span>';
+		$simle   = '<div id="simle_yan" class="mt-1 d-flex flex-wrap">' . "\r\n" . '%1$s</div>' . "\r\n";
+		$simle_a = '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="得意"><(￣︶￣)></span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="乾杯">[]~(￣▽￣)~*</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="啊啊">w(ﾟДﾟ)w</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="擦眼泪">(ノへ￣、)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="不屑">(￣_,￣ )</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="好耶">ヽ(✿ﾟ▽ﾟ)ノ</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="棒">(๑•̀ㅂ•́)و✧</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="抽">(￣ε(#￣)☆╰╮o(￣皿￣///)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="喂">(#`O′)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="亲">（づ￣3￣）づ╭❤～</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="汗">Σ( ° △ °|||)︴</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="笨">(～￣(OO)￣)ブ</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="擦">凸(艹皿艹 )</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="啵啵">(* ￣3)(ε￣ *)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="挖鼻屎">(*￣rǒ￣)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="嗷">┗|｀O′|┛ 嗷~~</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="好滴">(u‿ฺu✿ฺ)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="飞">︿(￣︶￣)︿</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="hi">Hi~ o(*￣▽￣*)ブ</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="啦啦">♪(^∇^*)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="拍桌">o(*≧▽≦)ツ┏━┓</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="惊喜">╰(*°▽°*)╯</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="嘟嘴">（○｀ 3′○）</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="乖">o(*^＠^*)o</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="愣住">(°ー°〃)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="放屁">○|￣|_ =3</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="哼哼">o(￣ヘ￣o＃)</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="可恶">（＝。＝）</span>' . "\r\n";
 		$simle   = sprintf( $simle, $simle_a );
 
 		return $simle;
@@ -48,9 +48,11 @@ if ( ! function_exists( 'simle_yan' ) ) {
 }
 if ( ! function_exists( 'simle_emoji' ) ) {
 	function simle_emoji() {
-		$simle   = '<div id="simle_emoji" class="mt-1 d-flex flex-wrap">%1$s</div>';
-		$simle_a = '<span class="mr-1 mt-1 p-1 rounded border border-secondary btn btn-sm text-muted" title="1">1</span>';
-		$simle_a .= '<span class="mr-1 mt-1 p-1 rounded border border-secondary btn btn-sm text-muted" title="2">2</span>';
+		$simle   = '<div id="simle_emoji" class="mt-1 d-flex flex-wrap">' . "\r\n" . '%1$s</div>' . "\r\n";
+		$simle_a = '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="😁">😁</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="😂">😂</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="😃">😃</span>' . "\r\n";
+		$simle_a .= '<span class="mr-1 mt-2 p-1 rounded border border-secondary btn btn-sm text-muted hvr-float-shadow" title="😄">😄</span>' . "\r\n";
 		$simle   = sprintf( $simle, $simle_a );
 
 		return $simle;
