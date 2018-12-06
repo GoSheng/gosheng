0d87a1c2841cbf3a0342ffb92250047a78748118
lin <469946668@qq.com>
2018-12-06 18:12:07 +0800

    改用Gitee提供版本更新

--- inc/theme_update.php
+++ inc/theme_update.php
@@ -7,7 +7,7 @@ if ( file_exists( get_theme_file_path() . '/inc/update/theme-update.php' ) ) {
 }
 
 $GoShengUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://raw.githubusercontent.com/gosheng/gosheng_update/master/theme.json',
+	'https://gitee.com/GoSheng/gosheng_update/raw/master/theme_back.json',
 	dirname( dirname( __FILE__ ) ) . '/functions.php',
 	'gosheng'
 );
