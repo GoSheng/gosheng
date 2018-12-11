c1db2418fade6c3cf0662cf79cc465ffaf9b8ae7
lin <469946668@qq.com>
2018-12-11 11:57:17 +0800

    搜索结果页面增加显示结果数量

--- search.php
+++ search.php
@@ -11,7 +11,14 @@ get_header();
     </div>
 <?php
 if ( have_posts() ) :?>
-    <div class="container bg-light py-3"><?php
+    <div class="container bg-light py-3">
+    <div><p><?php
+			echo '为您找到 ';
+			global $wp_query;
+			echo $wp_query->found_posts;
+			echo ' 个结果。';
+			?></p></div>
+	<?php
 	while ( have_posts() ) : the_post();
 		get_template_part( 'template-parts/other/page', 'list' );
 	endwhile;
@@ -20,7 +27,13 @@ if ( have_posts() ) :?>
 else:
 	?>
     <div class="container bg-light py-3">
-        <p><?php esc_html_e( '没有搜索到结果', 'GoSheng-framework' ); ?></p>
+        <p><?php
+	        echo '为您找到 ';
+	        global $wp_query;
+	        echo $wp_query->found_posts;
+	        echo ' 个结果。';
+	        ?></p>
+        <p><?php esc_html_e( '没有搜索到结果。', 'GoSheng-framework' ); ?></p>
     </div>
 <?php
 endif;
